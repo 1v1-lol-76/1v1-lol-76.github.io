e50110211fbd38aff79f05f12b67a9081f45d974
1v1-lol-76 <hakanlimak@yandex.com.tr>
2024-09-05 22:44:24 +0300

    Update g.js

--- Assests/JS/g.js
+++ Assests/JS/g.js
@@ -22,7 +22,7 @@ async function addGames() {
       project.addEventListener("click", () => {
 		  
     	console.log("acilis");
-		
+		googletag.pubads().refresh();
 	
         HAF.forEach((element) => element.classList.add("hidden"));
         Frame.classList.remove("hidden");
